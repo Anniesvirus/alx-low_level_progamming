af4ed4b185b03a9ce726db6dac1d1d0d40b180d8
Anniesvirus <aniefiokpatrick94@gmail.com>
2022-10-17 12:14:59 -0700

    Hello, put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'This program would print an information'
+ *
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	char Anniesvirus[] = "\"programming is like building a multilingual puzzle";
+
+	puts(Anniesvirus);
+
+	return (0);
+
+}
