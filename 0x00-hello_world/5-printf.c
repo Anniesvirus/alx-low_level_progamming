f684af249f64a35372fa598bee7ede034a79c20a
Anniesvirus <aniefiokpatrick94@gmail.com>
2022-10-17 13:06:33 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: 'This program will print an information using printf'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
